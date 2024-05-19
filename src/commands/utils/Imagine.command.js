c7631b6ab9d87a1d4ea8825a1caf9a145439faa5
tysaiwofc <mestre032021@gmail.com>
2024-05-18 20:09:22 -0400

    gemini

--- src/commands/utils/Imagine.command.js
+++ src/commands/utils/Imagine.command.js
@@ -1,13 +1,13 @@
 
 import { ActionRowBuilder, AttachmentBuilder, ButtonBuilder } from "discord.js";
 import Command from "../../structures/Command.js";
-import generateImage from "../../utils/generateModelExecute.js";
+import { generate } from "../../utils/geminiModelExecute.js";
 
 export default class extends Command {
     constructor(client) {
         super(client, {
             name: 'imagine',
-            description: "make AI create pornographic images for you",
+            description: "Ask Gemini questions",
             nsfw: true,
             integrationTypes: [0, 1],
             contexts: [0, 1, 2],
@@ -15,7 +15,7 @@ export default class extends Command {
                 {
                     type: 3,
                     name: "prompt",
-                    description: "describe what you want your image to look like",
+                    description: "Type in a question and it will answer you",
                     required: true,
                     minLength: 5,
                     maxLength: 80
@@ -30,19 +30,18 @@ export default class extends Command {
         const prompt = interaction.options.getString('prompt')
 
         const txt = interaction?.guild?.name || interaction?.user?.username
-        const buffer = await generateImage(prompt, txt).catch((er) => { console.log(er)})
-        if(!buffer) return interaction.editReply({ content: 'It looks like something went wrong when trying to generate this image, please try again later.'})
-        const image = new AttachmentBuilder(buffer, { name: 'porn.png'})
-
+        const buffer = await generate(prompt).catch(() => {})
+        if(!buffer) return interaction.editReply({ content: 'It looks like something went wrong when trying to generate response, please try again later.'})
+ 
         const btn = new ButtonBuilder()
-            .setLabel('Generate by Private AI')
+            .setLabel(txt)
             .setCustomId('genrat')
             .setDisabled(true)
             .setStyle('Primary')
         
         const row = new ActionRowBuilder()
             .addComponents(btn)
-        await interaction.editReply({ files: [image], components: [row] })
+        await interaction.editReply({ content: `${buffer}`, components: [row] })
 
     }
 }
